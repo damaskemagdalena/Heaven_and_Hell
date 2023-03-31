26e5751f8004511ee327974a75da0a3eac95d021
misia <damaske.magdalena@wp.pl>
2023-03-31 11:48:19 +0200

    Added student table

--- models.py
+++ models.py
@@ -1,3 +1,18 @@
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+class Student(Base):
+    __tablename__ = "student"
+
+    id = Column(Integer, primary_key = True, autoincrement = True)
+    first_name = Column(String(20), nullable = False)
+    last_name = Column(String(20), nullable=False)
+    PESEL = Column(String(20), nullable = False)
+    phone = Column(String(20), nullable=False)
+    address = Column(String(50), nullable=False)
+
+    def __repr__(self):
+        return f'Student({self.id}, {self.first_name}, {self.last_name}, {self.PESEL}, {self.phone}, {self.address})'
+
